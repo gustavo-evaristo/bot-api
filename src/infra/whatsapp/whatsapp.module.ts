44bc8923adc1e54a555d1fb47a71beeaa6676fbd
gustavo <gug.henri1@gmail.com>
2026-03-30 12:14:54 -0300

    feat: add auth module

--- src/infra/whatsapp/whatsapp.module.ts
+++ src/infra/whatsapp/whatsapp.module.ts
@@ -4,9 +4,10 @@ import { WhatsappGateway } from './whatsapp.gateway';
 import { WhatsappController } from './whatsapp.controller';
 import { DatabaseModule } from 'src/infra/database/database.module';
 import { ProcessMessageUseCase } from 'src/domain/use-cases/flow-engine/process-message.use-case';
+import { AuthenticationModule } from '../authentication/authentication.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthenticationModule],
   providers: [WhatsappService, WhatsappGateway, ProcessMessageUseCase],
   controllers: [WhatsappController],
 })
